d91fba8262f70cdd2878a91fbbff7b772ec13de7
Marcelo Jimenez Peña <marcelojp03@gmail.com>
2025-09-30 23:26:07 -0400

    se agrego swagger

--- be_condominium/be_condominium/urls.py
+++ be_condominium/be_condominium/urls.py
@@ -1,25 +1,22 @@
-"""
-URL configuration for be_condominium project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
+#from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from drf_spectacular.views import SpectacularAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ad/', include('modules.ad.urls')),  # Rutas de administración
-    path('api/ai/', include('modules.ai_security.urls')),# Rutas de IA
-    path('cn/', include('modules.cn.urls')),  # Rutas de Contabilidad
+    path('ad/', include('modules.ad.urls')),
+    path('api/ai/', include('modules.ai_security.urls')),
+    path('cn/', include('modules.cn.urls')),
+
+    # Swagger y Redoc
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
 ]
+
+ 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
